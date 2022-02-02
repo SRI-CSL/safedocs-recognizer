6998c9200dcb5d6e7e1eeff3c6fba06eab9c1530
Nick <32145399+nick-b-1@users.noreply.github.com>
2022-02-02 10:37:19 -0800

    Update filterfiles.py

--- components/consensus/filterfiles.py
+++ components/consensus/filterfiles.py
@@ -7,8 +7,9 @@ def genCoverageSummary(parserName):
     filetypes = [
         './**/*.c',
         './**/*.cc',
-        './**/*.cpp',
+        './**/*.cpp'
         './**/*.h',
+        './**/*.hh'
     ]
 
     files = []
@@ -20,15 +21,15 @@ def genCoverageSummary(parserName):
             ffilter = json.load(exclude)
             for d in ffilter['directories']:
                 if d == "": continue
-                files = filter(lambda x : not(x.startswith(d)),files)
+                files = list(filter(lambda x : not(x.startswith(d)),files))
             for f in ffilter['files']:
                 if f == "": continue
-                files = filter(lambda x : not(x.endswith(f)),files)
+                files = list(filter(lambda x : not(x.endswith(f)),files))
 
     results = { f : {'lines': sum(1 for _ in open(f,'rb'))} for f in files}
 
     with open('coverage-src-summary.json','w') as output:
-        json.dump(results,output, sort_keys=True,indent=2)
+        json.dump(results, output, sort_keys=True, indent=2)
 
 if __name__ == "__main__":
     genCoverageSummary(sys.argv[1] if len(sys.argv) == 2 else "")
